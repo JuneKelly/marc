f94a05e81982dc71810fc94b835e1e10eaf4733b
Shane Kilkelly <shane@kilkelly.me>
2016-10-29 16:02:59 +0100

    Page skeleton

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule Marc.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+
+  def status(conn, _params) do
+    text conn, "ok"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule Marc.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/about", PageController, :about
+    get "/status", PageController, :status
   end
 
   # Other scopes may use custom stacks.
