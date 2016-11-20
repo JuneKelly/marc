7b893c9e6ceb5aab30e5f4e9f4134bddaa0b4d98
Shane Kilkelly <shane@kilkelly.me>
2016-11-20 14:34:49 +0000

    Compile source to json

---
+++ tools/compiler.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import re
+import json
+
+
+# input
+MEDITATIONS_TEXT_PATH = "./resources/meditations.txt"
+
+
+# output
+MEDITATIONS_JSON_PATH = "./resources/meditations.json"
+MEDITATIONS_MARKDOWN_PATH = "./resources/meditations.md"
+
+
+def main():
+    print(">> Reading source text")
+    source_text = open(MEDITATIONS_TEXT_PATH, 'r').read()
+    source_text = re.sub("%%.*$", "", source_text, flags=re.MULTILINE)
+
+    print(">> splitting into books")
+    books = re.split(
+        "^########  BOOK \w+  ########",
+        source_text.strip(),
+        flags=re.MULTILINE
+    )[1:]
+
+    result = []
+
+    print(">> splitting into chapters")
+    for book_idx, book in enumerate(books):
+        chapter_list = []
+        chapters = re.split("--------", book, flags=re.MULTILINE)
+        for chapter_idx, chapter in enumerate(chapters):
+            chapter_list.append({
+                'book':     book_idx + 1,
+                'chapter':  chapter_idx + 1,
+                'text':     chapter.strip()
+            })
+        result.append(chapter_list)
+
+    print(">> Counts:")
+    for i, c in enumerate(result):
+        print("  Book {}, chapters: {}".format(i+1, len(c)))
+
+    print(">> writing to output files")
+    json_data = json.dumps(result, indent=2)
+
+    with open(MEDITATIONS_JSON_PATH, 'w') as outfile:
+        outfile.write(json_data)
+
+
+if __name__ == '__main__':
+    main()
