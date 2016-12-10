359265a5d2780eb0fb8c7a35e7f90cf12e1bcffe
Shane Kilkelly <shane@kilkelly.me>
2016-12-10 13:10:22 +0000

    Progress

--- lib/marc/meditations.ex
+++ lib/marc/meditations.ex
@@ -6,16 +6,20 @@ defmodule Marc.Meditations do
 
   # Public API
 
-  def random_chapter do
+  def random_chapter() do
     GenServer.call __MODULE__, :random
   end
 
-  def random_short_chapter do
+  def random_short_chapter() do
     GenServer.call __MODULE__, :random_short_chapter
   end
 
   def get_chapter_by_number(num) do
     GenServer.call __MODULE__, {:get_chapter_by_number, num}
+  end
+
+  def meditations() do
+    GenServer.call __MODULE__, :meditations
   end
 
   # GenServer API
@@ -37,9 +41,16 @@ defmodule Marc.Meditations do
       |> Enum.map(    fn({s, i}) -> {String.length(s.text), i} end )
       |> Enum.filter( fn({sl, _}) -> sl < @short_chapter_max_length end )
       |> Enum.map(    fn({_, i}) -> i+1 end )
-    {:ok, %{chapters: chapters,
-            short_chapter_numbers: short_chapter_numbers,
-            count: Enum.count(chapters)}}
+    markdown_text = Meditations.Data.meditations_to_markdown(meditations_data)
+    {:ok,
+     %{raw_json: raw,
+       meditations: meditations_data,
+       markdown_text: markdown_text,
+       flat: %{
+         chapter_list: chapters,
+         short_chapter_numbers: short_chapter_numbers,
+         count: Enum.count(chapters)
+       }}}
   end
 
   def handle_call({:get_chapter_by_number, num}, _from, state) do
@@ -47,29 +58,39 @@ defmodule Marc.Meditations do
     {:reply, chapter, state}
   end
 
-  def handle_call(:random, _from, %{count: count}=state) do
+  def handle_call(:meditations, _from, %{meditations: meditations}=state) do
+    {:reply, meditations, state}
+  end
+
+  def handle_call(:random, _from, state) do
+    %{flat: %{count: count}} = state
     num = :rand.uniform(count)
     random_chapter = get_chapter_view(state, num)
     {:reply, random_chapter, state}
   end
 
   def handle_call(:random_short_chapter, _from, state) do
-    %{short_chapter_numbers: short_chapter_numbers} = state
+    %{flat: %{short_chapter_numbers: short_chapter_numbers}} = state
     num = Enum.random(short_chapter_numbers)
     random_short_chapter = get_chapter_view(state, num)
     {:reply, random_short_chapter, state}
   end
 
   # Private Helpers
-  defp get_chapter_view(%{chapters: chapters, count: count}, chapter_num) do
-    case Enum.at(chapters, chapter_num-1) do
-      nil ->
+  defp get_chapter_view(%{flat: %{chapter_list: chapters, count: count}}, chapter_num) do
+    case chapter_num <= 0 do
+      true ->
         nil
-      chapter ->
-        %{chapter: chapter,
-          chapter_number: chapter_num,
-          previous: if(chapter_num == 1,         do: nil, else: chapter_num - 1),
-          next:     if(chapter_num == count, do: nil, else: chapter_num + 1)}
+      _ ->
+        case Enum.at(chapters, chapter_num-1) do
+          nil ->
+            nil
+          chapter ->
+            %{chapter: chapter,
+              chapter_number: chapter_num,
+              previous: if(chapter_num == 1,         do: nil, else: chapter_num - 1),
+              next:     if(chapter_num == count, do: nil, else: chapter_num + 1)}
+        end
     end
   end
 
--- lib/marc/meditations/data.ex
+++ lib/marc/meditations/data.ex
@@ -4,5 +4,23 @@ defmodule Meditations.Data do
     List.flatten(meditations)
   end
 
+  def chapter_to_markdown(chapter) do
+    "### #{chapter.book}-#{chapter.chapter}\n\n#{chapter.text}"
+  end
+
+  def book_to_markdown({book, book_index}) do
+    book_number = book_index + 1
+    "## Book #{book_number}" <> Enum.map_join book, "\n\n\n", &chapter_to_markdown/1
+  end
+
+  def meditations_to_markdown(meditations) do
+    link = Application.get_env(:marc, :base_url)
+    "# Marcus Aurelius - Meditations\n\n\nFrom #{link}\n\n--------\n\n" <> (
+      meditations
+      |> Enum.with_index()
+      |> Enum.map_join("\n\n\n", &book_to_markdown/1)
+    ) <> "\n\n--------"
+  end
+
 
 end
