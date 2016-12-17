f5bfeb5ce913615da4ac5942e5d9fc9c6980ac65
Shane Kilkelly <shane@kilkelly.me>
2016-12-17 13:22:43 +0000

    Bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Marc.Mixfile do
 
   def project do
     [app: :marc,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
