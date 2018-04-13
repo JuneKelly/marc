8ee86ad1517667a34e706597abaf9ed346971d31
Shane Kilkelly <shane@kilkelly.me>
2018-04-13 11:46:54 +0100

    update phoenix and elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Marc.Mixfile do
   def project do
     [app: :marc,
      version: "1.0.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -29,7 +29,7 @@ defmodule Marc.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.2"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
